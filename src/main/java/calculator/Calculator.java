cd04fc404311acb5aaf438a0b03ea05037ba7d5f
Nazanin Azarian <nazhixx@gmail.com>
2023-11-30 23:14:50 +0330

    Implement calculator customFunc

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,4 +4,8 @@ public class Calculator {
     public int add(int value1, int value2) {
         return value1 + value2;
     }
+
+    public int customFunc(int value1, int value2) {
+        return (int) Math.sqrt((double) value1 /value2);
+    }
 }
